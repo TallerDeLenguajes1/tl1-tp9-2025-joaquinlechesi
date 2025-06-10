bf562d2ff9ff13a331202ff73cf1c0af8ce4404d
joaquinlechesi <joaquinlechesi@gmail.com>
2025-06-10 12:33:19 -0300

    GetDirectories

--- LectorDirectorio/Program.cs
+++ LectorDirectorio/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+string path;
+Console.WriteLine("Ingrese la direccion/path que desee manipular:");
+path = Console.ReadLine();
+do
+{
+    if (!File.Exists(path))
+    {
+        Console.WriteLine("El directorio no existe.");
+    }
+    else
+    {
+        // Directory.GetDirectories(path);
+        string[] directorios = Directory.GetDirectories(path);
+        Console.WriteLine("Subdirectorios:");
+        for (int i = 0; i < directorios.Length; i++)
+        {
+            Console.WriteLine(directorios[i]);
+        }
+    }
+} while (!File.Exists(path));
